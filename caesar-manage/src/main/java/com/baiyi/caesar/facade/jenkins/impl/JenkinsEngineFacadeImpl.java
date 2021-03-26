dbc1d2010afd8a8b16bfcd3499f713d10833eb92
白衣 <liangjian@xinc818.group>
2021-03-26 11:22:25 +0800

    引擎视图代码优化

--- caesar-manage/src/main/java/com/baiyi/caesar/facade/jenkins/impl/JenkinsEngineFacadeImpl.java
+++ caesar-manage/src/main/java/com/baiyi/caesar/facade/jenkins/impl/JenkinsEngineFacadeImpl.java
@@ -9,6 +9,7 @@ import com.baiyi.caesar.jenkins.handler.JenkinsServerHandler;
 import com.baiyi.caesar.service.jenkins.CsJenkinsInstanceService;
 import com.baiyi.caesar.util.JobBuildUtils;
 import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import com.offbytwo.jenkins.helper.JenkinsVersion;
 import com.offbytwo.jenkins.model.Computer;
 import com.offbytwo.jenkins.model.ComputerWithDetails;
@@ -20,7 +21,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Author baiyi
@@ -50,22 +53,31 @@ public class JenkinsEngineFacadeImpl implements JenkinsEngineFacade {
     @Override
     @Cacheable(cacheNames = CachingConfig.CacheRepositories.ENGINE_CHART)
     public EngineVO.Children buildEngineChart() {
+        List<EngineVO.Children> instances = buildInstances();
         EngineVO.Children chart = EngineVO.Children.builder()
                 .name(ENGINES)
+                .children(instances)
+                .value(instances.size())
                 .build();
-        csJenkinsInstanceService.queryAll().forEach(e -> {
-            EngineVO.Children instance = EngineVO.Children.builder()
-                    .name(buildEngineName(e, e.getIsActive()))
-                    .build();
-            if (e.getIsActive()) {
-                assembleComputer(e, instance);
-                instance.setValue(instance.getChildren().size());
-            }
-            chart.addChildren(instance);
-        });
-        chart.setValue(chart.getChildren().size());
         JenkinsEngineFacadeImpl.chart = chart;
         return chart;
+    }
+
+    private List<EngineVO.Children> buildInstances() {
+        return csJenkinsInstanceService.queryAll().stream().map(i -> {
+            if (i.getIsActive()) {
+                List<EngineVO.Children> computers = buildComputers(i);
+                return EngineVO.Children.builder()
+                        .name(buildEngineName(i, i.getIsActive()))
+                        .children(computers)
+                        .value(computers.size())
+                        .build();
+            } else {
+                return EngineVO.Children.builder()
+                        .name(buildEngineName(i, i.getIsActive()))
+                        .build();
+            }
+        }).collect(Collectors.toList());
     }
 
     /**
@@ -88,33 +100,41 @@ public class JenkinsEngineFacadeImpl implements JenkinsEngineFacade {
         }
     }
 
-    private void assembleComputer(CsJenkinsInstance csJenkinsInstance, EngineVO.Children instance) {
+    private List<EngineVO.Children> buildComputers(CsJenkinsInstance csJenkinsInstance) {
+        List<EngineVO.Children> computers = Lists.newArrayList();
         try {
             Map<String, Computer> computerMap = jenkinsServerHandler.getComputerMap(csJenkinsInstance.getName());
             computerMap.keySet().forEach(k -> {
                 if (!k.equals(MASTER)) {
+                    List<EngineVO.Children> executors = buildExecutors(computerMap.get(k));
                     EngineVO.Children node = EngineVO.Children.builder()
                             .name(k)
+                            .children(executors)
+                            .value(executors.size())
                             .build();
-                    Computer computer = computerMap.get(k);
-                    try {
-                        ComputerWithDetails computerWithDetails = computer.details();
-                        computerWithDetails.getExecutors().forEach(e -> {
-                            EngineVO.Children executor = EngineVO.Children.builder()
-                                    .name(acqExecutorName(e))
-                                    .value(1)
-                                    .build();
-                            node.addChildren(executor);
-                        });
-                        node.setValue(node.getChildren().size());
-                        instance.addChildren(node);
-                    } catch (IOException ignored) {
-                    }
+                    computers.add(node);
                 }
             });
         } catch (Exception e) {
             log.error("组装Jenkins引擎工作负载错误!, err={}", e.getMessage());
         }
+        return computers;
+    }
+
+    private List<EngineVO.Children> buildExecutors(Computer computer) {
+        List<EngineVO.Children> executors = Lists.newArrayList();
+        try {
+            ComputerWithDetails computerWithDetails = computer.details();
+            computerWithDetails.getExecutors().forEach(e -> {
+                EngineVO.Children executor = EngineVO.Children.builder()
+                        .name(acqExecutorName(e))
+                        .value(1)
+                        .build();
+                executors.add(executor);
+            });
+        } catch (IOException ignored) {
+        }
+        return executors;
     }
 
     private String acqExecutorName(Executor executor) {
